812adf3a14a5e44f59aa2679b9825f1190fb6db0
ma4185 <maren.osterloh@jade-hs.de>
2015-06-09 16:32:17 +0200

    Update 09.06.15 - Buttons funktionieren

--- wave_infinity.m
+++ wave_infinity.m
@@ -1,37 +1,48 @@
 function wave_infinity()
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+clc
+clear 
+close all 
 
-L=10;
+L   = 10;
 sep = 4;
-N=500;
-k= 15;
-V=linspace(-L, L, N);
+N   = 500;
+k   = 10;
+
+V   = linspace(-L, L, N);
+
 [X, Y] = meshgrid(V, V);
+
 I=sqrt(-1);
-R1= sqrt( (X-sep).^2 + Y.^2 );
-R2= sqrt( (X+sep).^2 + Y.^2 );
-R3= sqrt( (Y+sep).^2 + (X+sep).^2 );
-R4= sqrt( (Y-sep).^2 + (X+sep).^2 );
-temp='2 Schallquellen';
-M=10;
-T=linspace(0.0, 2*pi, M); T=T(1:(M-1));
-cut = 0.8;
+R1  = sqrt( (X-sep).^2 + Y.^2 );
+R2  = sqrt( (X+sep).^2 + Y.^2 );
+R3  = sqrt( (Y+sep).^2 + (X+sep).^2 );
+R4  = sqrt( (Y-sep).^2 + (X+sep).^2 );
+
+Z = exp(I*k*R1)./R1 + exp(I*k*R2)./R2; %%??
+
+%temp='2 Schallquellen';
+M   = 10;
+T   = linspace(0.0, 2*pi, M); 
+T   =T(1:(M-1));
+cut = 0,8;
 scale = 30/(2*cut);
 
 %% buttons
 
-handles.popupmenu = uicontrol('Style','popupmenu',...
-          'position',[10 80 100 30], ...
-          'string',{'2 Schallquellen','3 Schallquellen','4 Schallquellen'},...
-          'callback',@(h,e)(sine_wave_a_Startbutton))
+handle.popupmenu = uicontrol('Style','popupmenu',...
+                    'position',[10 80 100 30], ...
+                    'string',{'2 Schallquellen','3 Schallquellen','4 Schallquellen'},...
+                    'callback',@(h,e)(number_source))
 
-handles.start_stop = uicontrol('position',[10 45 100 30], ...
-    'String','START', ...
-    'callback',@(h,e)(start_stop))
+handle.start_stop = uicontrol('position',[10 45 100 30], ...
+                    'String','START', ...
+                    'callback',@(h,e)(start_stop))
 
-% auswahl_schall=get(handle.popupmenu, 'string');
+
 % wahl_schall=get(handle.popupmenu, 'value');
+% auswahl_schall=get(handle.popupmenu, 'string');
 % chosen_schall=auswahl_schall(wahl_schall);
 
 
@@ -50,27 +61,35 @@ handles.start_stop = uicontrol('position',[10 45 100 30], ...
     end
 
     function start_stop()
-        set(gcbo,'String',setdiff({'START' 'STOP'},get(gcbo,'String')));
-        while strcmp('STOP',get(gcbo,'String'))
-            
-            for iter=1:length(T)
-                
-                W = real(Z*exp(-I*T(iter)));
-                W = max(W, -cut);
-                W = min(W, cut);
-                
-                image(scale*(W+cut));
-                axis equal; axis off;
-                drawnow;
-                
-                pause(0.01);
-            end
-        end
         
+        %switch %%handel kram
+            %case 
+                set(gcbo,'String',setdiff({'START' 'STOP'},get(gcbo,'String')));
+                while strcmp('STOP',get(gcbo,'String'))
+                    
+                    for iter=1:length(T)
+                        
+                        W = real(Z*exp(-I*T(iter)));
+                        W = max(W, -cut);
+                        W = min(W, cut);
+                        
+                        image(scale*(W+cut));
+                        axis equal; axis off;
+                        drawnow;
+                        
+                        pause(0.01);
+                    end
+                end
+        %end
     end
 
     function number_source()
-        switch temp
+        
+        wahl_schall=get(handle.popupmenu, 'value');
+        auswahl_schall=get(handle.popupmenu, 'string');
+
+        
+        switch auswahl_schall{wahl_schall}
             case '2 Schallquellen'
                 Z = exp(I*k*R1)./R1 + exp(I*k*R2)./R2;
 
@@ -81,8 +100,9 @@ handles.start_stop = uicontrol('position',[10 45 100 30], ...
                 Z = exp(I*k*R1)./R1 + exp(I*k*R2)./R2+ exp(I*k*R3)./R3+ exp(I*k*R4)./R4;
 
         end
-        
+
     end
+
 
 
 end
