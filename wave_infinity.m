5152c8d60b5f18ccc2fd2d5b2db5b6fe67a2325b
ma4185 <maren.osterloh@jade-hs.de>
2015-06-16 13:08:56 +0200

    Endversion 16.06.2015

--- wave_infinity.m
+++ wave_infinity.m
@@ -1,4 +1,4 @@
-function wave_infinity()
+function wave_infinity_TEST3()
 % Diese Funktion erstellt eine 2-dimensionale Grafik in der sich Schallwellen kreisförmig ausbreiten und Interferenzmuster zeigen.
 % Anwendung: wave_infinity
 % Eingegebener Parameter:
@@ -7,57 +7,58 @@ function wave_infinity()
 %           Grafik mit verschiedenen Anzahlen an Schallquellen, mit der
 %           variablen Einstellung von Amplitude und Frequenz
 
-% Authoren: Nora Jürgens, Hanna Möhle, M. Osterloh
+% Authoren: Nora Jürgens, Hanna Möhle, Maren Osterloh
 %           (Jade Hochschule, 2. Prüfungsleistung Daten und Algorithmen)
 % Version:  1.0 programmiert am 15.06.2015 NJ HM MO
 %           dokumentiert am 15.06.2015 NJ HM MO
-% 
+%
 % wichtiger Hinweis: Grundlage des Codes beruht auf folgender Quelle: Oleg Alexandro, File:Two sources interference.gif , erstellt am
 %          03:11, 13 January 2008 (UTC), URL: http://commons.wikimedia.org/wiki/File:Two_sources_interference.gif, zuletzt
 %          besucht am 15.06.2015 13:54
-
 
 clc
 clear
 close all
 
-% Variablen anlegen
-L   = 10;
-sep = 4;
-N   = 500;
+%% Variablen anlegen
 
+% feste Variablen
+L      = 10;
+sep    = 4;
+N      = 500;
 freq   = 10;
-x_koor=[];
-y_koor=[];
-W=[];
-wave_ampl=[];
-x=[];
-idx=[];
-
 V   = linspace(-L, L, N);
 [X, Y] = meshgrid(V, V);
 I=sqrt(-1);
-
-%Positionen der Schallquellen anlegen
-R1  = sqrt( (X-sep).^2 + Y.^2 );
-R2  = sqrt( (X+sep).^2 + Y.^2 );
-R3  = sqrt( (Y+sep).^2 + (X+sep).^2 );
-R4  = sqrt( (Y-sep).^2 + (X-sep).^2 );
-
-Z = exp(I*freq*R1)./R1+ exp(I*freq*R2)./R2;
-
 M   = 30;
 T   = linspace(0.0, 2*pi, M);
 T   =T(1:(M-1));
 amp = 0.8;
 scale = 30/(2*amp);
-time=(0:0.01:5);
+idx=1;
+time=(0:0.01:5); % Zeitvektor zur Erstellung des Linienplots
+
+% leere Variablen
+x_koor=[];
+y_koor=[];
+W=[];
+wave_ampl=[];
+x=[];
+
+% Positionen der Schallquellen anlegen
+R1  = sqrt( (X-sep).^2 + Y.^2 );
+R2  = sqrt( (X+sep).^2 + Y.^2 );
+R3  = sqrt( (Y+sep).^2 + (X+sep).^2 );
+R4  = sqrt( (Y-sep).^2 + (X-sep).^2 );
+
+% Verknüpfung der Schallquellen in der Matrix
+Z = exp(I*freq*R1)./R1+ exp(I*freq*R2)./R2;
 
 
-%% buttons
+%% Buttons
 
-% Erstellen von Buttons zur Einstellung der Frequenz, Amplitude und der
-% Anzahl der Schallquellen
+% Erstellen von Buttons zur Einstellung der Frequenz, Amplitude, der
+% Anzahl der Schallquellen und zum Starten und Stoppen der Animation
 
 handle.start_stop = uicontrol('position',[10 45 100 30], ...
     'String','START', ...
@@ -75,7 +76,7 @@ handle.frequency = uicontrol('Style', 'slider',...
 
 handle.freq_text = uicontrol('style', 'text', ...
     'position', [20 150 100 20], ...
-    'string', '3.5');
+    'string', '10');
 
 handle.amplitude = uicontrol('Style', 'slider',...
     'Min',0.1,'Max',2,'Value',0.8,...
@@ -94,12 +95,13 @@ handle.freq_descript = uicontrol('style', 'text', ...
     'position', [10 150 60 20], ...
     'string', 'Frequency:');
 
-idx=1;
 draw()
 
-%%
+%% Optische Darstellung
+
 % Funktion ist für die Berechnung der Werte und für die Darstellung in der
 % Animation zuständig (Hauptfunktion, zur Darstellung des Bildes)
+
     function draw(iter)
         if nargin == 0
             iter = 1;
@@ -128,8 +130,10 @@ draw()
         
     end
 
-%%
+%% Steuerung der Animation
+
 % Der Start/Stop-Botton startet oder stoppt die Animation
+
     function start_stop()
         
         set(gcbo,'String',setdiff({'START' 'STOP'},get(gcbo,'String')));
@@ -145,31 +149,35 @@ draw()
         
     end
 
-%%
+%% Anzahl der Schallquellen
+
 % Anzahl der Schallquellen kann ausgewählt werden und wird in der Berechnung zur Animation
 % der Graphik geändert
+
     function number_source()
         
         wahl_schall=get(handle.popupmenu, 'value');
         auswahl_schall=get(handle.popupmenu, 'string');
         
         switch auswahl_schall{wahl_schall}
-            case '2 Schallquellen'
+            case '2 sound sources'
                 Z = exp(I*freq*R1)./R1 + exp(I*freq*R2)./R2;
                 
-            case '3 Schallquellen'
+            case '3 sound sources'
                 Z = exp(I*freq*R1)./R1 + exp(I*freq*R2)./R2+ exp(I*freq*R3)./R3;
                 
-            case '4 Schallquellen'
+            case '4 sound sources'
                 Z = exp(I*freq*R1)./R1 + exp(I*freq*R2)./R2+ exp(I*freq*R3)./R3+ exp(I*freq*R4)./R4;
                 
         end
         
     end
 
-%%
+%% Frequenzeinstellung
+
 % Frequenz kann ausgewählt werden und wird in der Berechnung zur Animation
 % der Graphik geändert
+
     function frequency()
         
         freq = get(handle.frequency, 'value');
@@ -179,9 +187,11 @@ draw()
         
     end
 
-%%
+%% Amplitudeneinstellung
+
 % Amplitude kann ausgewählt werden und wird in der Berechnung zur Animation
 % der Graphik geändert
+
     function amplitude()
         
         amp = get(handle.amplitude, 'value');
@@ -190,9 +200,11 @@ draw()
         draw()
     end
 
-%%
+%% Auswahl des Wellenpunktes
+
 % Punkt für die lokale Wellenform kann ausgewählt werden
-    function addpoint(h, event)
+
+    function addpoint(~, event)
         
         koor=event.IntersectionPoint;
         x_koor=round(koor(1));
@@ -201,4 +213,5 @@ draw()
         
         
     end
+
 end
